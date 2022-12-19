8eab68fab9585935c438152fdb81dfd38d5154f1
unknown <praveenkumar619.sp@gmail.com>
2022-12-19 22:51:55 +0530

    added code for tests order of execution and priority concept

--- src/test/java/com/testng/basics/Basics03_TestNG_PriorityConcept.java
+++ src/test/java/com/testng/basics/Basics03_TestNG_PriorityConcept.java
@@ -4,10 +4,13 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Basics02_TestNGTests_OrderOfExecution {
+public class Basics03_TestNG_PriorityConcept {
 	
 	/**
-	 * Test cases will be executed based on the method names(alphabetical order)
+	 * NOTE : We can also set priority to our test cases, it will accept integer values.
+	 * If we set the integer value to negative then it will be executed first.
+	 * If both the tests having same priority then it will pick based on the alphabetical order.
+	 * If we don't set any priority then the non priority test cases will be executed.
 	 */
 	
 	@BeforeTest
@@ -15,27 +18,27 @@ public class Basics02_TestNGTests_OrderOfExecution {
 		System.out.println("Browser is opened");
 	}
 	
-	@Test
+	@Test(priority = 4)
 	public void aTest() {
 		System.out.println(" A Test");
 	}
 	
-	@Test
+	@Test(priority = 5)
 	public void bTest() {
 		System.out.println(" B Test");
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void cTest() {
 		System.out.println(" C Test");
 	}
 	
-	@Test
+	@Test(priority = 3)
 	public void dTest() {
 		System.out.println(" D Test");
 	}
 	
-	@Test
+	@Test(priority = 1)
 	public void eTest() {
 		System.out.println(" E Test");
 	}
